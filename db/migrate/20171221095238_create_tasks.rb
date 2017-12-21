a099e92feb44c342396ee9bf400af2cbf962f4f1
Definence <dp210592ybo@gmail.com>
2017-12-21 17:10:45 +0200

    first commit

---
+++ db/migrate/20171221095238_create_tasks.rb
@@ -0,0 +1,14 @@
+class CreateTasks < ActiveRecord::Migration[5.0]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.string :description
+      t.integer :priority
+      t.boolean :active
+      t.date :due_date
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
