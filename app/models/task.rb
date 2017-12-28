686cad8dd71f8966d9d64350c8860acb2456b511
Definence <dp210592ybo@gmail.com>
2017-12-28 17:27:34 +0200

    -

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,7 @@
 class Task < ApplicationRecord
 
+  validates :title, presence: true, length: { minimum: 5, too_short: "Five characters is the minimum allowed" }
+  validates :priority, presence: true, numericality: { only_integer: true }
+  validates :due_date, presence: true
+
 end
