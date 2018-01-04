8a8f7bd1c276b4e0e4962b84cce54351f73c18bd
Definence <dp210592ybo@gmail.com>
2018-01-04 15:00:44 +0200

    validations

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,13 +1,25 @@
 class Task < ApplicationRecord
 
-  validates :title, presence: true, length: {
-  	minimum: 5,
-  	maximum: 30,
-  	too_long: "Thirty characters is the maximum allowed",
-  	too_short: "Five characters is the minimum allowed"
-  }
+	# checked
+  validates :title, presence: true,
+	  length: {
+	  	minimum: 5,
+	  	maximum: 30,
+	  	too_long: "Thirty characters is the maximum allowed",
+	  	too_short: "Five characters is the minimum allowed"
+	  }
 
-  validates :priority, presence: true, numericality: { only_integer: true }
+  # checked
+	validates :description, presence:true
+
+	# checked
+  validates :priority, presence: true,
+	  numericality: {
+	  	only_integer: true,
+	  	less_than_or_equal_to: 9999,
+	  }
+
+  #checked
   validates :due_date, presence: true
 
 end
