c424667aa520721ae5fdf77bc04e17127e81745d
Definence <dp210592ybo@gmail.com>
2018-01-29 10:29:51 +0200

    serializer fixes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,20 +4,21 @@ class SessionsController < ApplicationController
   expose :user, -> { User.find_by(email: session_params[:email].downcase) }
 
   def create
-    # шукає юзера по імейлу(інпут з бекенду) і назначає в перемінну
     # user = User.find_by(email: params[:session][:email].downcase)
-    # перевіряє наявність юзера і перевіряє відповідність паролей
-    if user && user.authenticate(session_params[:password])
-      # виводить дані на фронтенд
-      if user.confirmed
-        render_api(user, :ok, serializer: SessionSerializer)
-        # замінили на serializer
-        # render json: { username: user.username, token: user.token, message: 'Login successfully' }, status: 200
-      elsif !user.confirmed
-        render json: { message: 'Confirm your email' }, status: 207
+
+    if user
+      if user.authenticate(session_params[:password])
+        if user.confirmed
+          render_api(user, 200, serializer: SessionSerializer)
+        else
+          render json: { message: 'Confirm your email' }, status: 207
+        end
+      else
+        render json: { message: 'Wrong email or password' }, status: 400
       end
     else
-      render json: { message: 'Invalid email or password.' }, status: 204
+      render json: { message: 'User does not exist' }, status: 204
+      # render_api({ message: I18n.t('User does not exist') }, 204)
     end
   end
 
--- app/serializers/session_serializer.rb
+++ app/serializers/session_serializer.rb
@@ -1,4 +1,3 @@
 class SessionSerializer < ActiveModel::Serializer
   attributes :username, :token
-  # attributes :id, :email, :username, :firstname, :lastname, :token
 end
