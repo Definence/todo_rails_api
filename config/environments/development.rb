d5e596d3448c3b9ae77fd4d04468ec9b2bf2738f
Definence <dp210592ybo@gmail.com>
2018-02-06 11:56:06 +0200

    mailer fixes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,7 +46,7 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = {:host => 'localhost:3000'}
+  config.action_mailer.default_url_options = {:host => 'https://todo-react-api.herokuapp.com/'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
